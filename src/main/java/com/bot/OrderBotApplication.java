51b8af29ae7e5c9df9b8e1661d8d7a2f79ce5515
Ashutosh Sharma <ashu.the.engg@gmail.com>
2017-08-15 16:27:52 +0530

    Initial Commit

---
+++ src/main/java/com/bot/OrderBotApplication.java
@@ -0,0 +1,12 @@
+package com.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderBotApplication.class, args);
+	}
+}
